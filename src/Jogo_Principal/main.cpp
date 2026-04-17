85df2ca199a8c73f9bac3cbea3757424c6ed7e25
Chan Hong Lik <chanlik@alunos.utfpr.edu.br>
2026-04-17 18:18:07 -0300

    Add project files.

---
+++ src/Jogo_Principal/main.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Henrique on 17/04/2026.
+//
+
+#include "main.h"
+
---
+++ src/Jogo_Principal/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Henrique on 17/04/2026.
+//
+
+#ifndef JOGO_MAIN_H
+#define JOGO_MAIN_H
+
+#endif //JOGO_MAIN_H
